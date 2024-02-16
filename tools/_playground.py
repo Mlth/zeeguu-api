0ff88868c92066fd32e73dc9408765c79ea3c9ea
oskaitu <100945285+oskaitu@users.noreply.github.com>
2024-02-16 02:18:24 +0100

    playing around with function

--- tools/_playground.py
+++ tools/_playground.py
@@ -2,11 +2,11 @@ import sys
 
 from elasticsearch import Elasticsearch
 from zeeguu.core.elastic.settings import ES_CONN_STRING, ES_ZINDEX
-from zeeguu.core.model import UserExerciseSession, User
-import pandas as pd
+from zeeguu.core.model import UserExerciseSession, User, UserReadingSession, Article, UserLanguage
+# import pandas as pd
 from zeeguu.core.model import db
-import matplotlib.pyplot as plt
-import numpy as np
+# import matplotlib.pyplot as plt
+# import numpy as np
 
 from zeeguu.api.app import create_app
 from zeeguu.core.model.user_reading_session import UserReadingSession
@@ -14,9 +14,68 @@ from zeeguu.core.model.user_reading_session import UserReadingSession
 app = create_app()
 app.app_context().push()
 
-'''
-print("before the for")
-for id in User.all_recent_user_ids(150):
+
+def isArticleLiked():
+    user = 534
+    
+
+    articleData = {}
+
+    language_data = {
+    2: {'name': 'Danish', 'avrt': 204},
+    3: {'name': 'German', 'avrt': 179},
+    5: {'name': 'English', 'avrt': 228},
+    7: {'name': 'French', 'avrt': 195},
+    6: {'name': 'Spanish', 'avrt': 218},
+    8: {'name': 'Italian', 'avrt': 188},
+    9: {'name': 'Dutch', 'avrt': 202},
+    10: {'name': 'Norwegian', 'avrt': 204},
+    11: {'name': 'Portuguese', 'avrt': 181},
+    13: {'name': 'Polish', 'avrt': 166},
+    18: {'name': 'Swedish', 'avrt': 204},
+    19: {'name': 'Russian', 'avrt': 180},
+    23: {'name': 'Hungarian', 'avrt': 161},
+    }
+
+    readingSession = ( 
+        UserReadingSession.query
+            .filter_by(user_id=user)
+            .filter(UserReadingSession.article_id.isnot(None))
+            .order_by(UserReadingSession.article_id.asc())
+            .limit(100)
+    )
+    for session in readingSession:
+        articleId = session.article_id
+        article = Article.find_by_id(articleId)
+        sessionDuration = int(session.duration) / 1000
+        if articleId not in articleData:
+            articleData[articleId] = {
+                'duration': sessionDuration,
+                'language': article.language_id,
+                'difficulty': article.fk_difficulty,
+                'wordCount': article.word_count,
+                'likeScore': 0
+            }
+        else:
+            articleData[articleId]['duration'] += sessionDuration
+
+    
+    for article in articleData:
+        user_level = UserLanguage.query.filter_by(user_id = user, language_id=articleData[article]['language']).first()
+        averageShouldSpend = articleData[article]['wordCount'] / language_data[articleData[article]['language']]['avrt']
+       
+
+        articleData[article]['likeScore'] = int( 
+            (articleData[article]['duration'] / averageShouldSpend ) 
+            * (articleData[article]['difficulty'] / user_level.cefr_level)
+        )
+    return articleData
+
+print("before the function")
+print(isArticleLiked())
+
+# print("before the for")
+""" for id in User.all_recent_user_ids(150):
     u = User.find_by_id(id)
     print(u.name)
     duration_old = exercises_duration_by_day(u)
@@ -25,12 +84,12 @@ for id in User.all_recent_user_ids(150):
         print("old way")
         print(duration_old)
         print("new way")
-        print(duration_new)
-'''
+        print(duration_new) """
 
-print("before test")
 
-conn = db.engine.raw_connection()
+# print("before test")
+
+#conn = db.engine.raw_connection()
 
 query = """
     SELECT * from user_reading_session urs
@@ -65,7 +124,7 @@ upper_bound = True
 lower_bound = True
 
 y_start = 50
-
+""" 
 df = pd.read_sql_query(query, conn)
 #df.to_csv(sys.stdout, index=False)
 df.astype('int32').dtypes
@@ -89,7 +148,7 @@ plt.show()
 
 conn.close()
 
-print("after test")
+print("after test") """
 
 '''
 def initialize_all_focused_durations():
