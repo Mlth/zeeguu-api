6ecc260b2446aad0844311f0e02f6a0d14dc1103
Mlth <mlth@itu.dk>
2024-02-21 12:46:22 +0100

    Made nice plot

--- tools/_playground.py
+++ tools/_playground.py
@@ -27,38 +27,106 @@ def days_since_to_opacity(days_since):
         return 0.25
     return 0
 
-def translated_words_per_article(userId, articleId):
-    activitySession = UserActivityData.query.filter_by(user_id=userId, article_id=articleId).all()
+def translated_words_per_article(user_id, article_id):
+    activitySession = UserActivityData.query.filter_by(user_id=user_id, article_id=article_id).all()
     count = 0
     for activity in activitySession:
         if activity.event == "UMR - TRANSLATE TEXT":
             count += 1
     return count
 
-def get_expected_reading_time(word_count):
-    return (word_count / 70) * 60
+def get_expected_reading_time(word_count, offset):
+    # The higher the offset is, the higher we want the WPM to be. When WPM is larger, the user is expected to be able to read faster.
+    # Thus, high offset/WPM = low expected reading time.
+    return (word_count / (70 + offset)) * 60
 
-def plot_urs_with_duration_and_word_count(df, file_name):
+def plot_urs_with_duration_and_word_count(df, have_read_sessions, file_name):
     x_min, x_max = 0, 2000
     y_min, y_max = 0, 2000
 
-    df.plot(kind = 'scatter', x = 'word_count', y = 'user_duration')
+    plt.xlabel('Word count')
+    plt.ylabel('Duration')
 
-    plt.scatter(df['word_count'], df['user_duration'], alpha=[days_since_to_opacity(d) for d in df['days_since']], color='blue')
+    dot_color = np.where(df['haveRead'] == 1, 'blue', 'red')
+    plt.scatter(df['word_count'], df['user_duration'], alpha=[days_since_to_opacity(d) for d in df['days_since']], color=dot_color)
 
     x_values = df['word_count']
-    y_values_line = [get_expected_reading_time(x) - 120 for x in x_values]
+    y_values_line = [get_expected_reading_time(x, -20) for x in x_values]
     plt.plot(x_values, y_values_line, color='red', label='y = ')
 
     x_values = df['word_count']
-    y_values_line = [get_expected_reading_time(x) + 120 for x in x_values]
+    y_values_line = [get_expected_reading_time(x, 20) for x in x_values]
     plt.plot(x_values, y_values_line, color='red', label='y = ')
+
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
+    plt.grid(True)
+    plt.rc('axes', axisbelow=True)
+
+    have_read_ratio = have_read_sessions / len(df) * 100
+    have_not_read_ratio = 100 - have_read_ratio
+    plt.text(0, 1.1, f"Have read: {have_read_ratio:.2f}%", transform=plt.gca().transAxes)
+    plt.text(0, 1.05, f"Have not read: {have_not_read_ratio:.2f}%", transform=plt.gca().transAxes)
 
     plt.savefig(file_name + '.png')
     print("Saving file: " + file_name + ".png")
     plt.show()
+
+def get_all_user_reading_sessions():
+    sessions = {}
+    have_read_sessions = 0
+
+    query_data = (
+        UserReadingSession.query
+            .filter(UserReadingSession.article_id.isnot(None))
+            .filter(UserReadingSession.duration >= 30000) # 30 seconds
+            .filter(UserReadingSession.duration <= 3600000) # 1 hour
+            .filter(UserReadingSession.start_time >= datetime.now() - timedelta(days=365)) # 1 year
+            .order_by(UserReadingSession.user_id.asc())
+            .all()
+    )
+
+    for session in query_data:
+        article_id = session.article_id
+        user_id = session.user_id
+        article = Article.find_by_id(article_id)
+        session_duration = int(session.duration) / 1000 # in seconds
+
+        if (user_id, article_id) not in sessions:
+            sessions[(user_id, article_id)] = {
+                'user_duration': session_duration,
+                'language': article.language_id,
+                'difficulty': article.fk_difficulty,
+                'word_count': article.word_count,
+                'haveRead': 0,
+                'days_since': (datetime.now() - session.start_time).days,
+            }
+        else:
+            sessions[(user_id, article_id)]['user_duration'] += session_duration
+    
+    for session in sessions.keys():
+        user_id = session[0]
+        article_id = session[1]
+
+        should_spend_reading_lower_bound = get_expected_reading_time(sessions[session]['word_count'], 20)
+        should_spend_reading_upper_bound = get_expected_reading_time(sessions[session]['word_count'], -20)
+
+        timesTranslated = translated_words_per_article(user_id, article_id)
+        userDurationWithTranslated = sessions[session]['user_duration'] - (timesTranslated * 3)
+        
+        if userDurationWithTranslated <= should_spend_reading_upper_bound and userDurationWithTranslated >= should_spend_reading_lower_bound:
+            have_read_sessions += 1
+            sessions[session]['haveRead'] = 1
+        else:
+            sessions[session]['haveRead'] = 0
+
+    return sessions, have_read_sessions
+
+def get_df_and_plot_user_reading_sessions():
+    sessions, have_read_sessions = get_all_user_reading_sessions()
+    df = pd.DataFrame.from_dict(sessions, orient='index')
+    plot_urs_with_duration_and_word_count(df, have_read_sessions, "test")
+    return df
 
 # This function makes a dataframe for one user with 
 def isArticleLiked():
@@ -68,19 +136,19 @@ def isArticleLiked():
 
     # Not using this, new information found, average reading time for learners is lower.
     """ language_data = {
-    2: {'name': 'Danish', 'avrt': 204}, # taken from norwegian
-    3: {'name': 'German', 'avrt': 179},
-    5: {'name': 'English', 'avrt': 228},
-    7: {'name': 'French', 'avrt': 195},
-    6: {'name': 'Spanish', 'avrt': 218},
-    8: {'name': 'Italian', 'avrt': 188},
-    9: {'name': 'Dutch', 'avrt': 202},
-    10: {'name': 'Norwegian', 'avrt': 204},
-    11: {'name': 'Portuguese', 'avrt': 181},
-    13: {'name': 'Polish', 'avrt': 166},
-    18: {'name': 'Swedish', 'avrt': 204}, # taken from norwegian
-    19: {'name': 'Russian', 'avrt': 180}, # i made it
-    23: {'name': 'Hungarian', 'avrt': 161}, # taken from finnish
+        2: {'name': 'Danish', 'avrt': 204}, # taken from norwegian
+        3: {'name': 'German', 'avrt': 179},
+        5: {'name': 'English', 'avrt': 228},
+        7: {'name': 'French', 'avrt': 195},
+        6: {'name': 'Spanish', 'avrt': 218},
+        8: {'name': 'Italian', 'avrt': 188},
+        9: {'name': 'Dutch', 'avrt': 202},
+        10: {'name': 'Norwegian', 'avrt': 204},
+        11: {'name': 'Portuguese', 'avrt': 181},
+        13: {'name': 'Polish', 'avrt': 166},
+        18: {'name': 'Swedish', 'avrt': 204}, # taken from norwegian
+        19: {'name': 'Russian', 'avrt': 180}, # i made it
+        23: {'name': 'Hungarian', 'avrt': 161}, # taken from finnish
     } """
 
     averageReadingTime = 70 # wpm
@@ -91,7 +159,6 @@ def isArticleLiked():
             .filter(UserReadingSession.article_id.isnot(None))
             .filter(UserReadingSession.duration >= 30000) # 30 seconds
             .filter(UserReadingSession.duration <= 3600000) # 1 hour
-            .filter(UserReadingSession.start_time >= datetime.now() - timedelta(days=365)) # 1 year
             .order_by(UserReadingSession.article_id.asc())
             .all()
     )
@@ -118,7 +185,7 @@ def isArticleLiked():
     for article in articleData:
         # Not using this right now, but it could be useful
         # user_level = UserLanguage.query.filter_by(user_id = user, language_id=articleData[article]['language']).first()
-        averageShouldSpendReading = get_expected_reading_time(articleData[article]['word_count'])
+        averageShouldSpendReading = get_expected_reading_time(articleData[article]['word_count'], 0)
 
         timesTranslated = translated_words_per_article(user, article)
         userDurationWithTranslated = articleData[article]['user_duration'] - (timesTranslated * 3)
@@ -143,91 +210,8 @@ def isArticleLiked():
     return df
 
 print("before the function")
-plot_urs_with_duration_and_word_count(isArticleLiked(), "oscar_test")
-#print(translated_words_per_article(534, 294067))
 
-# print("before the for")
-""" for id in User.all_recent_user_ids(150):
-    u = User.find_by_id(id)
-    print(u.name)
-    duration_old = exercises_duration_by_day(u)
-    duration_new = exercises_duration_by_day(u)
-    if duration_new != duration_old:
-        print("old way")
-        print(duration_old)
-        print("new way")
-        print(duration_new) """
+get_df_and_plot_user_reading_sessions()
+#plot_urs_with_duration_and_word_count(isArticleLiked(), "oscar_test")
 
-
-# print("before test")
-
-conn = db.engine.raw_connection()
-
-query = """
-    SELECT * from user_reading_session urs
-    LEFT JOIN article a ON urs.article_id = a.id
-    WHERE urs.duration / a.word_count > 0.1
-"""
-
-query = """
-    SELECT
-        (urs.duration / 60 / 60) AS duration,
-        a.word_count
-    FROM
-        user_reading_session urs
-    LEFT JOIN
-        article a ON urs.article_id = a.id
-    JOIN
-        (SELECT article_id, COUNT(*) AS session_count
-        FROM user_reading_session
-        GROUP BY article_id
-        HAVING COUNT(*) >= 20) AS session_counts
-    ON
-        urs.article_id = session_counts.article_id
-    WHERE
-        urs.duration IS NOT NULL
-        AND a.word_count IS NOT NULL
-        AND urs.duration / 60 / 60 < 60
-        AND urs.duration / 60 / 60 >= 2
-        AND a.word_count < 2000
-"""
-
-upper_bound = True
-lower_bound = True
-
-y_start = 50
-"""
-df = pd.read_sql_query(query, conn)
-#df.to_csv(sys.stdout, index=False)
-df.astype('int32').dtypes
-df.plot(kind = 'scatter', x = 'duration', y = 'word_count', color='blue')
-
-if upper_bound:
-    x_values = df['duration']
-    y_values_line = 20 * x_values + y_start
-    plt.scatter(df['duration'], df['word_count'], label='Data Points')
-    plt.plot(x_values, y_values_line, color='red', label='y = 2x + 2')
-
-if lower_bound:
-    x_values = df['duration']
-    y_values_line = [y_start] * len(x_values)
-    plt.scatter(df['duration'], df['word_count'], label='Data Points')
-    plt.plot(x_values, y_values_line, color='red', label='y = 2x + 2')
-
-plt.savefig('test.png')
-print("Has been saved")
-plt.show()
-
-"""
-conn.close()
 print("after test")
-
-'''
-def initialize_all_focused_durations():
-    for session in db['user_reading_session'].all():
-
-        
-
-def initialize_focused_duration(user_id, article_id):
-    return
-'''
