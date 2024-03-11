314b66d98741db626ba7bcc5fcba340c7693b7b3
nselpriv <nicolaiselo@gmail.com>
2024-03-11 15:54:48 +0100

    moved candidate generator

--- tools/_playground.py
+++ tools/_playground.py
@@ -7,7 +7,7 @@ import pandas as pd
 from zeeguu.core.model import db
 import pyarrow as pa # needed for pandas
 from zeeguu.api.app import create_app
-from zeeguu.core.candidate_pool_generator.candidate_generator import build_candidate_pool_for_lang, build_candidate_pool_for_user, initial_candidate_pool
+from zeeguu.recommender.candidate_generator import build_candidate_pool_for_lang, build_candidate_pool_for_user, initial_candidate_pool
 from zeeguu.recommender.feedback_matrix import AdjustmentConfig, FeedbackMatrix, FeedbackMatrixConfig, ShowData
 from zeeguu.core.elastic.elastic_query_builder import build_elastic_search_query as ElasticQuery
 from zeeguu.core.elastic.indexing import index_all_articles
